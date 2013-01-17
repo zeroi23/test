328207b954eaf2fcd4893bdd4f3a26e26ae63874
zeroi23 <zongchaomail@gmail.com>
2013-01-17 10:57:28 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,5 @@
-javascript:document.getElementsByClassName('input_detail')[0].value="2B?";document.getElementsByClassName('send_btn')[0].click();
+function test_s{
+  document.getElementsByClassName('input_detail')[0].value="2B?";
+  document.getElementsByClassName('send_btn')[0].click();
+}
+test_s();
